ea0165b4e935e9ee524654c33d81ccd9fcb9b6e3
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-01-16 18:11:53 +0100

    create header and code 50%

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
 import {  Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 const Layout = () => {
     return (
         <main>
+            <Header/>
             <div className="content">
                 <Outlet/>
             </div>
