854328b913a5806eabe9988d62a3582a83d21a52
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-01-23 14:36:18 +0100

    we handle pagination routes of links (page/1)

--- src/components/Homepage/Products/pagination/PaginationPart.jsx
+++ src/components/Homepage/Products/pagination/PaginationPart.jsx
@@ -1,17 +1,35 @@
 //@ts-nocheck
 import Pagination from "react-bootstrap/Pagination";
 import styles from "./Pagination.module.scss";
-import { NavLink, generatePath } from "react-router-dom";
+import { NavLink, generatePath, useNavigate } from "react-router-dom";
 import Routes from "../../../../Routes/Routes";
+import classNames from "classnames";
+
+{
+  /* <NavLink to={generatePath(Routes.Page, { number: page })}>
+{page}
+</NavLink> */
+}
 
 const PaginationPart = ({ pageCount }) => {
+  const navigate = useNavigate();
+
+  const handleChangePage = (href) => (e) => {
+    e.preventDefault();
+    navigate(href);
+  };
+
   const pages = [];
   for (let page = 1; page <= pageCount; page++) {
+    const href = generatePath(Routes.Page, { number: page });
     pages.push(
-      <Pagination.Item key={page} className={styles.borderColor}>
-        <NavLink to={generatePath(Routes.Page, { number: page })}>
-          {page}
-        </NavLink>
+      <Pagination.Item
+        key={page}
+        className={styles.borderColor}
+        href={href}
+        onClick={handleChangePage(href)}
+      >
+        {page}
       </Pagination.Item>
     );
   }
@@ -20,7 +38,9 @@ const PaginationPart = ({ pageCount }) => {
     <Pagination className="sticky-bottom">
       {pages}
       <Pagination.Next className={styles.borderColor} />
-      <Pagination.Last className={"rounded-end" styles.borderColor} />
+      <Pagination.Last
+        className={classNames("rounded-end", styles.borderColor)}
+      />
     </Pagination>
   );
 };
