1a0b9ce4ea62315323652f9a88770f0130598e36
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-02-13 11:49:42 +0100

    add ts to productFilters.ts

--- src/store/states/productFilters.ts
+++ src/store/states/productFilters.ts
@@ -1,8 +1,15 @@
-//@ts-nocheck
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "..";
+
+//TYPES
+interface InitialState {
+  search: string;
+  categoryId?: number;
+  minPrice?: number;
+  maxPrice?: number;
+}
 //initialStates
-const initialState = {
+const initialState: InitialState = {
   search: "",
   categoryId: undefined,
   minPrice: undefined,
@@ -15,30 +22,36 @@ const slice = createSlice({
   initialState,
   reducers: {
     //actionNAme : reducer
-    setSearch: (state, action) => {
+    setSearch: (state, action: PayloadAction<InitialState["search"]>) => {
       state.search = action.payload;
     },
-    setCategoryId: (state, action) => {
+    setCategoryId: (
+      state,
+      action: PayloadAction<InitialState["categoryId"]>
+    ) => {
       state.categoryId = action.payload;
     },
-    setMinPrice: (state, action) => {
+    setMinPrice: (state, action: PayloadAction<InitialState["minPrice"]>) => {
       state.minPrice = action.payload;
     },
-    setMaxPrice: (state, action) => {
+    setMaxPrice: (state, action: PayloadAction<InitialState["maxPrice"]>) => {
       state.maxPrice = action.payload;
     },
   },
 });
 export default slice.reducer;
 
-export const { setSearch, setCategoryId, setMaxPrice, setMinPrice } = slice.actions;
-  
+export const { setSearch, setCategoryId, setMaxPrice, setMinPrice } =
+  slice.actions;
 
 export const { name } = slice;
 
 //selectors
 
 export const selectSearch = (state: RootState) => state.productFilters.search;
-export const selectCategoryId = (state: RootState) => state.productFilters.categoryId;
-export const selectMaxPrice = (state: RootState) => state.productFilters.maxPrice;
-export const selectMinPrice = (state: RootState) => state.productFilters.minPrice;
+export const selectCategoryId = (state: RootState) =>
+  state.productFilters.categoryId;
+export const selectMaxPrice = (state: RootState) =>
+  state.productFilters.maxPrice;
+export const selectMinPrice = (state: RootState) =>
+  state.productFilters.minPrice;
