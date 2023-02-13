a22c92106a2b77a990b1e9a795e473fa9137f4ea
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-02-13 11:06:48 +0100

    improve files ...

--- src/components/Homepage/Aside/Aside.tsx
+++ src/components/Homepage/Aside/Aside.tsx
@@ -1,20 +1,13 @@
-//@ts-nocheck
-import { Col, Row } from "react-bootstrap";
-import { useAppDispatch, useAppSelector } from "../../../store/hooks";
-import { selectIsLoading } from "../../../store/states/isLoading";
-import { selectPageCount } from "../../../store/states/pageCount";
 import CategoriesFilter from "./category/CategoriesFilter";
 import FilteredPrice from "./price/FilteredPrice";
 
-
-const Aside = () =>{
-    
-    return (
-        <aside>
-            <CategoriesFilter/>
-            <FilteredPrice/>
-        </aside>
-    );
-}
+const Aside = () => {
+  return (
+    <aside>
+      <CategoriesFilter />
+      <FilteredPrice />
+    </aside>
+  );
+};
 
 export default Aside;
