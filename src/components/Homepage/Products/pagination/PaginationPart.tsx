1fb27e106b3b24348f5c18227c5a4c2ed0a20faa
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-02-14 11:20:32 +0100

    add ts to PaginationPart.tsx  _

--- src/components/Homepage/Products/pagination/PaginationPart.tsx
+++ src/components/Homepage/Products/pagination/PaginationPart.tsx
@@ -1,35 +1,44 @@
-//@ts-nocheck
-import Pagination from "react-bootstrap/Pagination";
-import styles from "./Pagination.module.scss";
-import { NavLink, generatePath, useNavigate } from "react-router-dom";
-import Routes from "../../../../Routes/Routes";
+import { generatePath, useNavigate } from "react-router-dom";
+import { MouseEvent, ReactElement } from "react";
 import classNames from "classnames";
+
+import styles from "./Pagination.module.scss";
+import Routes from "../../../../Routes/Routes";
+import Pagination from "react-bootstrap/Pagination";
 
 {
   /* <NavLink to={generatePath(Routes.Page, { number: page })}>
 {page}
 </NavLink> */
 }
+interface PaginationPartProps {
+  pageCount: number;
+  currentPage?: number;
+}
 
-const PaginationPart = ({ pageCount , currentPage=1}) => {
+const PaginationPart = ({
+  pageCount,
+  currentPage = 1,
+}: PaginationPartProps) => {
   const navigate = useNavigate();
-  
 
-  const handleChangePage = (href) => (e) => {
-    e.preventDefault();
-    navigate(href);
-  };
+  const handleChangePage =
+    (href: string) =>
+    (e: MouseEvent<HTMLLinkElement>): void => {
+      e.preventDefault();
+      navigate(href);
+    };
 
-  const pages = [];
+  const pages: ReactElement[] = [];
   for (let page = 1; page <= pageCount; page++) {
-    const href = generatePath(Routes.Page, { number: page });
+    const href = generatePath(Routes.Page, { number: page.toString() });
     pages.push(
       <Pagination.Item
         key={page}
         className={styles.borderColor}
         href={href}
         onClick={handleChangePage(href)}
-        active={currentPage===page}
+        active={currentPage === page}
       >
         {page}
       </Pagination.Item>
