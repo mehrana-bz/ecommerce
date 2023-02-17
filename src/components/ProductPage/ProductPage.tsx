130a69b861467be83a6bd3e9926cfb515c041801
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-02-17 12:47:14 +0100

    first step  _ we fetch id from url with useParams  _we fetch data from backend  we have clickedproduct informetion with useState  now we are going to set them in new Page

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,5 +1,22 @@
-function ProductPage() {
-  return <div>daff</div>;
+import axios from "axios";
+import { useEffect , useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../store/states/products";
+
+const ProductPage = () => {
+    const {id} = useParams();
+    const [product,setProduct] = useState<Product>();
+
+    useEffect(() => {
+        axios.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`)
+        .then(res => res.data)
+        .then((fetchedProduct) => setProduct(fetchedProduct));
+    },[])
+    return(
+        <>
+          
+        </>
+    )
 }
 
 export default ProductPage;
