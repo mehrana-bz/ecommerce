d5341b6183d0a97616108cf253211334510e5b74
mehrana bahrami <mehranabahramizadeh@gmail.com>
2023-03-16 11:32:23 +0100

    I changed header codes with responsive codes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Link, generatePath, useNavigate } from "react-router-dom";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import { Button, Container, Navbar, Form } from "react-bootstrap";
+import { Button, Container, Navbar, Form, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ChangeEvent, FormEvent } from "react";
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
@@ -17,6 +17,7 @@ import LogoIcon from "../icons/LogoIcon";
 import styles from "./Header.module.scss";
 import { selectBookmarks } from "../../store/states/bookmarks";
 import { selectShoppingCart } from "../../store/states/shoppingCart";
+import classNames from "classnames";
 
 const Header = () => {
   const dispatch = useAppDispatch();
@@ -41,56 +42,113 @@ const Header = () => {
   const shoppingCartState = useAppSelector(selectShoppingCart);
 
   const shoppingCartsSum = useMemo(() => {
-    return shoppingCartState.reduce((sum , cart) => sum + cart.count, 0)
-  },[shoppingCartState]);
+    return shoppingCartState.reduce((sum, cart) => sum + cart.count, 0);
+  }, [shoppingCartState]);
 
   return (
+    // <header id={styles.Header}>
+    //   <Navbar bg="light" variant="light" fixed="top" expand="lg">
+    //     <Container>
+    //       <Navbar.Brand as={Link} to={Routes.Homepage} className="Websitelogo">
+    //         <LogoIcon />
+    //       </Navbar.Brand>
+    //       <Navbar.Toggle aria-controls="navbar-dark-example" />
+    //       <div className="d-flex align-items-center w-100 justify-content-end gap-3">
+    //         <Form onSubmit={handleSearchSubmit} className="d-flex w-50">
+    //           <Form.Control
+    //             type="search"
+    //             placeholder="Search"
+    //             className="me-2"
+    //             aria-label="Search"
+    //             onInput={handleSearchValueInput}
+    //             value={search}
+    //           />
+    //           <Button variant="outline-primary" type="submit">
+    //             Search
+    //           </Button>
+    //         </Form>
+    //         <Link to={Routes.Bookmarks}>
+    //           <div className="position-relative">
+    //             <FontAwesomeIcon icon={faHeart} className="fs-4" />
+    //             {bookmarksState.length !== 0 && (
+    //               <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    //                 {bookmarksState.length}
+    //               </span>
+    //             )}
+    //           </div>
+    //         </Link>
+    //         <Link to={Routes.ShoppingCart}>
+    //           <div className="position-relative">
+    //             <FontAwesomeIcon
+    //               icon={faBasketShopping}
+    //               className="fs-5 text-primary"
+    //               role="button"
+    //             />
+    //             {shoppingCartState.length !== 0 && (
+    //               <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    //                 {shoppingCartsSum}
+    //               </span>
+    //             )}
+    //           </div>
+    //         </Link>
+    //       </div>
+    //     </Container>
+    //   </Navbar>
+    // </header>
+
     <header id={styles.Header}>
-      <Navbar bg="light" variant="light" fixed="top">
+      <Navbar bg="light" variant="light" fixed="top" expand="lg">
         <Container>
           <Navbar.Brand as={Link} to={Routes.Homepage} className="Websitelogo">
             <LogoIcon />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-dark-example" />
-          <div className="d-flex align-items-center w-100 justify-content-end gap-3">
-            <Form onSubmit={handleSearchSubmit} className="d-flex w-50">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                onInput={handleSearchValueInput}
-                value={search}
-              />
-              <Button variant="outline-primary" type="submit">
-                Search
-              </Button>
-            </Form>
-            <Link to={Routes.Bookmarks}>
-              <div className="position-relative">
-                <FontAwesomeIcon icon={faHeart} className="fs-4" />
-                {bookmarksState.length !== 0 && (
-                  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    {bookmarksState.length}
-                  </span>
-                )}
+          <Navbar.Toggle aria-controls="navbar-dark-example basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
+            <Nav className="me-auto">
+              <div className="d-flex align-items-center w-100 justify-content-end gap-3">
+                <Form
+                  onSubmit={handleSearchSubmit}
+                  className={classNames(styles.form, "d-flex")}
+                >
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    onInput={handleSearchValueInput}
+                    value={search}
+                  />
+                  <Button variant="outline-primary" type="submit">
+                    Search
+                  </Button>
+                </Form>
+                <Link to={Routes.Bookmarks}>
+                  <div className="position-relative">
+                    <FontAwesomeIcon icon={faHeart} className="fs-4" />
+                    {bookmarksState.length !== 0 && (
+                      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {bookmarksState.length}
+                      </span>
+                    )}
+                  </div>
+                </Link>
+                <Link to={Routes.ShoppingCart}>
+                  <div className="position-relative">
+                    <FontAwesomeIcon
+                      icon={faBasketShopping}
+                      className="fs-5 text-primary"
+                      role="button"
+                    />
+                    {shoppingCartState.length !== 0 && (
+                      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {shoppingCartsSum}
+                      </span>
+                    )}
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to={Routes.ShoppingCart}>
-              <div className="position-relative">
-                <FontAwesomeIcon
-                  icon={faBasketShopping}
-                  className="fs-5 text-primary"
-                  role="button"
-                />
-                {shoppingCartState.length !== 0 && (
-                  <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    {shoppingCartsSum}
-                  </span>
-                )}
-              </div>
-            </Link>
-          </div>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
